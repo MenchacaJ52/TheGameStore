ca7d3901238a63c7bc596db1c23a3cca80ed3a64
Kevinsancue <sanchezcuek@jbu.edu>
2025-12-07 13:28:31 -0600

    Add files via upload

---
+++ Da game store/Da game store/Services/CartService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Da_game_store.Services
+{
+    public class CartService
+    {
+        public List<Game> Items { get; } = new();
+
+        public decimal Total => Items.Sum(i => i.Price);
+
+        public event Action? OnChange;
+
+        public void Add(Game g)
+        {
+            Items.Add(g);
+            OnChange?.Invoke();
+        }
+
+        public void Remove(Game g)
+        {
+            Items.Remove(g);
+            OnChange?.Invoke();
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+            OnChange?.Invoke();
+        }
+    }
+}
---
+++ Da game store/Da game store/Services/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Da_game_store.Services
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
